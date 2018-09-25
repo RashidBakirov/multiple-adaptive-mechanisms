343e70778529fae20eddc504055efa17a3fa63c5
brashid <isagbagi1>
2018-09-25 09:39:42 +0100

    Please entee the commit message for your changes. Lines starting

--- Code/SABLE/Recursive_no_scaling_intercept/createExpert.m
+++
@@ -1,8 +0,0 @@
-function [ expert ] = createExpert( B, P, INNER, Q)
-%CREATEEXPERT Summary of this function goes here
-        expert.B=B;
-        expert.P=P;
-        expert.Q=Q;
-        expert.INNER=INNER;
-end
-
