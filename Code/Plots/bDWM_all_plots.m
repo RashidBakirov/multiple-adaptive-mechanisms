83da0c57d4c825e1f556ef380df11caecbf80400
Rashid Bakirov <b.rashid@gmail.com>
2020-11-16 20:01:37 +0000

    deleting cuplicates

--- Code/Plots/bDWM_all_plots.m
+++
@@ -1,5 +0,0 @@
-bDWM_getresults;
-bDWM_forplots;
-cmacamp=colormap;
-bDWM_plots_synth;
-bDWM_plots_real;
--- Code/Plots/bPL_all_plots.m
+++
@@ -1,5 +0,0 @@
-bPL_getresults;
-bPL_forplots;
-cmacamp=colormap;
-bPL_plots_synth;
-bPL_plots_real;
