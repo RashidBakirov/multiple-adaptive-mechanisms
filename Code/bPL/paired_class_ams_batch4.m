75d85519371e10c0027b082858308a0f0bb15929
Rashid Bakirov <b.rashid@gmail.com>
2021-01-21 15:29:12 +0000

    PL adding no adaptation option and bug fix

--- Code/bPL/paired_class_ams_batch4.m
+++ Code/bPL/paired_class_ams_batch4.m
@@ -1,4 +1,4 @@
-function[result, preds, result_test, pred_test, avg_result_test, change_hist, change, avg_acc, avg_acc_test] = paired_class_ams_batch3(data, labels, threshold, classifier, mode, flag_rc,  batchsize, test_data, test_labels)
+function[result, preds, result_test, pred_test, avg_result_test, change_hist, change, avg_acc, avg_acc_test] = paired_class_ams_batch4(data, labels, threshold, classifier, mode, flag_rc,  batchsize, test_data, test_labels)
 
 % Simulation of simple online classfication without forgetting
 
@@ -9,7 +9,8 @@ NUMFOLDS=10;
 ds=dataset(data(1:batchsize,:),labels(1:batchsize)); %create the first datapoint
 ds_react=ds;
 exp=ds*classifier;
-exp_react=ds*classifier;
+exp_react=exp;
+exp_old=exp;
 
 avg_result_test=[];
 
@@ -43,27 +44,45 @@ for k = 2:floor(rows/batchsize)
 
     dsk=dataset(dataBatch,labelsBatch); 
     
-    tst=dsk*exp; %perform classification
+    
+    tst=dsk*exp; %perform classification (stable expert)
     tst_react=dsk*exp_react; %perform classification on reactive expert
-    fin_pred=labeld_rb(tst); %get predicted label
-    fin_pred_react=labeld_rb(tst_react); %get predicted label
+    tst_0=dsk*exp_old; %perform classification on "old" expert (no adaptation)
+    
+    fin_pred=labeld_rb(tst); %get predicted label (stable expert)
+    fin_pred_react=labeld_rb(tst_react); %get predicted label (reactive expert)
+    fin_pred_0=labeld_rb(tst_0); %get predicted label (no adaptation)
+    
     fin_acc=mean(fin_pred==labelsBatch);
     fin_acc_react=mean(fin_pred_react==labelsBatch);
-
+    fin_acc_0=mean(fin_pred_0==labelsBatch);
+    
     ds_old=ds;
     ds_old_react=ds_react;
+    exp_old=exp;
+    exp_react_old=exp_react;
+    
+    %no adaptation
+    if mode==0 | (mode==4 & xval_select==0)
+        preds(1+(k-2)*batchsize:(k-1)*batchsize)=fin_pred_0;
+        disp([num2str(k) ': AM deployed=0']);
     
     %stable learner
-    if mode==1 | (mode==4 & xval_select==1)
+    elseif mode==1 | (mode==4 & xval_select==1)
         preds(1+(k-2)*batchsize:(k-1)*batchsize)=fin_pred;
         ds=[ds; dsk];
+        ds_react = dsk;
         disp([num2str(k) ': AM deployed=1']);
-        
+        exp=ds*classifier; %retrain stable expert
+        exp_react=ds_react*classifier; %retrain reactive expert
     %reactive learner
     elseif mode==2 | (mode==4 & xval_select==2)
         preds(1+(k-2)*batchsize:(k-1)*batchsize)=fin_pred_react;
-        ds=[ds_react; dsk];
+        ds=[ds_react; dsk];   
         disp([num2str(k) ': AM deployed=2']);
+        ds_react = dsk;
+        exp=ds*classifier; %retrain stable expert
+        exp_react=ds_react*classifier; %retrain reactive expert
         
     %paired learner    
     elseif mode==3
@@ -78,27 +97,40 @@ for k = 2:floor(rows/batchsize)
             ds=[ds; dsk];
             preds(1+(k-2)*batchsize:(k-1)*batchsize)=fin_pred;
         end
+        ds_react = dsk;
+        exp=ds*classifier; %retrain stable expert
+        exp_react=ds_react*classifier; %retrain reactive expert
         
     %optimal learner    
     else
-        if fin_acc<fin_acc_react
-            preds(1+(k-2)*batchsize:(k-1)*batchsize)=fin_pred_react;
-            ds=[ds_react; dsk];
-        else
+        [~,max_ind] =max([fin_acc fin_acc_react fin_acc_0]);
+        if max_ind==1
             preds(1+(k-2)*batchsize:(k-1)*batchsize)=fin_pred;
             ds=[ds; dsk];
+            ds_react = dsk;
+            exp=ds*classifier; %retrain stable expert
+            exp_react=ds_react*classifier; %retrain reactive expert
+        elseif max_ind==2
+            fin_acc<fin_acc_react
+            preds(1+(k-2)*batchsize:(k-1)*batchsize)=fin_pred_react;
+            ds=[ds_react; dsk];
+            ds_react = dsk;
+            exp=ds*classifier; %retrain stable expert
+            exp_react=ds_react*classifier; %retrain reactive expert
+        else
+            preds(1+(k-2)*batchsize:(k-1)*batchsize)=fin_pred_0;
         end
     end
     
     if mode==4
-        [xval_select] = paired_xval(ds_old,ds_old_react,dataBatch,labelsBatch, classifier, batchsize, NUMFOLDS)
+        [xval_select] = paired_xval4(exp_old, ds_old,ds_old_react,dataBatch,labelsBatch, classifier, batchsize, NUMFOLDS);
     end
     
     ds_react = dsk;
-    
     exp=ds*classifier; %retrain stable expert
-    exp_react=ds_react*classifier; %retrain stable expert
+    exp_react=ds_react*classifier; %retrain reactive expert
     
+       
     result(1+(k-2)*batchsize:(k-1)*batchsize) = (preds(1+(k-2)*batchsize:(k-1)*batchsize)==labelsBatch');
 
     
@@ -113,19 +145,27 @@ for k = 2:floor(rows/batchsize)
         changecounter=0;
     end
     
-
-    
-    
     if flag_rc
-        if fin_acc<fin_acc_react
-            disp([num2str(k) ': RC=2']);
-            ds=[ds_old_react; dsk];
-        elseif fin_acc>fin_acc_react
+        [~,max_ind] =max([fin_acc fin_acc_react fin_acc_0]);
+        if max_ind==1
             disp([num2str(k) ': RC=1']);
             ds=[ds_old; dsk];
+            ds_react = dsk;
+            exp=ds*classifier; %retrain stable expert
+            exp_react=ds_react*classifier; %retrain reactive expert
+        elseif max_ind==2
+            disp([num2str(k) ': RC=2']);
+            ds=[ds_old_react; dsk];
+            ds_react = dsk;
+            exp=ds*classifier; %retrain stable expert
+            exp_react=ds_react*classifier; %retrain reactive expert
+        else
+            disp([num2str(k) ': RC=0']);
+            ds=ds_old;
+            ds_react=ds_old_react;
+            exp=exp_old;
+            exp_react=exp_react_old;
         end
-        %exp=ds*classifier; %retrain stable expert
-        %exp_react=ds_react*classifier; %retrain stable expert
     end
     
 %    disp([num2str(k) ': AM deployed=' num2str(selected_index) ', AM optimal=' num2str(correct_index)]);
---
+++ Code/bPL/paired_xval4.m
@@ -0,0 +1,44 @@
+function[max_ind] = paired_xval4(exp0, ds,ds_react,dataBatch,labelsBatch, classifier, batchSize, numFolds)
+
+    testSize=round(batchSize/numFolds);
+    
+    sumAccuracy=zeros(1,2);
+
+    for i=1:numFolds
+
+        labelsBatchTest=labelsBatch(1+(i-1)*testSize:i*testSize);
+        dataBatchTest=dataBatch(1+(i-1)*testSize:i*testSize,:);
+        
+        labelsBatchTrain=[labelsBatch(1:1+(i-1)*testSize-1);labelsBatch(i*testSize+1:end)];
+        dataBatchTrain=[dataBatch(1:1+(i-1)*testSize-1,:);dataBatch(i*testSize+1:end,:)];
+        
+        dsk_train=dataset(dataBatchTrain,labelsBatchTrain);
+        dsk_test=dataset(dataBatchTest,labelsBatchTest);
+        
+        ds_i = [ds; dsk_train];
+        ds_react_i = [ds_react; dsk_train];
+        
+        exp=ds_i*classifier; %retrain stable expert
+        exp_react=ds_react_i*classifier; %retrain stable expert
+        
+        tst=dsk_test*exp; %perform classification
+        tst_react=dsk_test*exp_react; %perform classification on reactive expert
+        fin_pred=labeld_rb(tst); %get predicted label
+        fin_pred_react=labeld_rb(tst_react); %get predicted label
+        fin_acc=mean(fin_pred==labelsBatchTest);
+        fin_acc_react=mean(fin_pred_react==labelsBatchTest);
+        
+        sumAccuracy(1)=sumAccuracy(1)+fin_acc;
+        sumAccuracy(2)=sumAccuracy(2)+fin_acc_react;
+
+    end
+    tst0=dataBatch*exp; %perform classification
+    fin_pred0=labeld_rb(tst0); %get predicted label
+    fin_acc0=mean(fin_pred0==labelsBatch);
+    
+    [~,max_ind] =max([sumAccuracy fin_acc0*numFolds]);
+    
+    if max_ind==3
+        max_ind=0;
+    end
+end
