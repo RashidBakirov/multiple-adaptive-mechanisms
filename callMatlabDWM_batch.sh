f55c4f1c8ad7053175b01e2c3cceb732c1500975
RashidBakirov <37158588+RashidBakirov@users.noreply.github.com>
2021-06-06 13:27:37 +0100

    Update callMatlabDWM_batch.sh

--- callMatlabDWM_batch.sh
+++ callMatlabDWM_batch.sh
@@ -1,2 +1,2 @@
-matlab -r runBatchDynamicWeightedMajority_nb $1 $2" > dwm_nb_$1+$2.log
-matlab -r runBatchDynamicWeightedMajority_ht $1 $2" > dwm_nb_$1+$2.log
+matlab -r "runBatchDynamicWeightedMajority_nb $1 $2" > dwm_nb_$1+$2.log
+matlab -r "runBatchDynamicWeightedMajority_ht $1 $2" > dwm_nb_$1+$2.log
